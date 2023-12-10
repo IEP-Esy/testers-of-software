e93ca5e11befdeaec09ee3b4373258d56e6f6541
Gimantha Dissanayake <gimantha.dissanayake@tuni.fi>
2023-12-10 17:54:32 +0200

    implemented unit tests for endsWith.js

---
+++ tests/endsWith.test.js
@@ -0,0 +1,60 @@
+import endsWith from '../src/endsWith';
+
+describe('endsWith function', () => {
+    test('returns true if a product name ends with "Shoes"', () => {
+        const productName = 'Running Shoes';
+        const result = endsWith(productName, 'Shoes');
+        expect(result).toBe(true);
+    });
+
+    test('returns false if a product name does not end with "Hat"', () => {
+        const productName = 'Winter Jacket';
+        const result = endsWith(productName, 'Hat');
+        expect(result).toBe(false);
+    });
+
+    test('returns true if a URL ends with "/checkout"', () => {
+        const url = 'https://example.com/shop/checkout';
+        const result = endsWith(url, '/checkout');
+        expect(result).toBe(true);
+    });
+
+    test('returns true when checking an empty string for an empty target', () => {
+        const result = endsWith('', '');
+        expect(result).toBe(true);
+    });
+
+    test('returns true when checking a string with a length of 1 for an empty target', () => {
+        const result = endsWith('A', '');
+        expect(result).toBe(true);
+    });
+
+    test('returns false when checking an empty string for a non-empty target', () => {
+        const result = endsWith('', 'abc');
+        expect(result).toBe(false);
+    });
+
+    test('handles negative position correctly', () => {
+        const result = endsWith('abcdef', 'a', -3);
+        expect(result).toBe(false);
+    });
+
+    test('adjusts position to string length when position is greater', () => {
+        const inputString = 'abcdef';
+        const target = 'ef';
+
+        // Set position greater than the length of the string
+        const position = inputString.length + 2;
+
+        const result = endsWith(inputString, target, position);
+    
+        // Expect the function to adjust position to the length of the string
+        expect(result).toBe(true);
+    });
+
+    test('returns true if a SKU ends with "-XL"', () => {
+        const sku = 'ABC123-XL';
+        const result = endsWith(sku, '-XL');
+        expect(result).toBe(true);
+    });
+});
